9850af206084e7976b29568074d0b4f608cfee87
abdulmuizdev <devservices@abdulmuiz.dev>
2024-10-20 22:07:58 +0500

    Files Updates

--- App/yamaloader/build/web/flutter_bootstrap.js
+++ App/yamaloader/build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "713278815"
+    serviceWorkerVersion: "4092434342"
   }
 });
